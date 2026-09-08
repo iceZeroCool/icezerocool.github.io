a71b01f8a743aa36ffafa6a54a3879ae659154f0
zhouxuemei <zhouxuemei@xgd.com>
2026-09-08 20:44:12 +0800

    fix: 数据脚本加版本号参数破浏览器缓存 + SW v47

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "fcards-v46";
-const ASSETS = ["./index.html", "./manifest.json", "./icon-180.png", "./cards-data.js", "./lessons-data.js"];
+const CACHE = "fcards-v47";
+const ASSETS = ["./index.html", "./manifest.json", "./icon-180.png", "./cards-data.js?v=20260908", "./lessons-data.js?v=20260908"];
 self.addEventListener("install", (e) => {
   e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)).then(() => self.skipWaiting()));
 });
