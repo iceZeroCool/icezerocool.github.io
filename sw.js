be782ec9f699553ed4b5b3a0f231c86dd930dcab
zhouxuemei <zhouxuemei@xgd.com>
2026-09-10 18:23:25 +0800

    feat: 学习tab最新在前倒序(W1前W0后) + 回前台自动拉同步(+) + 文章进度云同步

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE = "fcards-v47";
-const ASSETS = ["./index.html", "./manifest.json", "./icon-180.png", "./cards-data.js?v=20260908c", "./lessons-data.js?v=20260908c"];
+const ASSETS = ["./index.html", "./manifest.json", "./icon-180.png", "./cards-data.js?v=20260908e", "./lessons-data.js?v=20260908e"];
 self.addEventListener("install", (e) => {
   e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)).then(() => self.skipWaiting()));
 });
