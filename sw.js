a4f0c01bbdc94ee48485f1569749e3387bd6cb21
zhouxuemei <zhouxuemei@xgd.com>
2026-09-11 15:49:33 +0800

    feat: 复习卡移除同步码——单用户码写死(sole-fuxika-cards)+启动即同步+去绑码界面；飞书数据已迁移到该码

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE = "fcards-v47";
-const ASSETS = ["./index.html", "./manifest.json", "./icon-180.png", "./cards-data.js?v=20260908e", "./lessons-data.js?v=20260908e"];
+const ASSETS = ["./index.html", "./manifest.json", "./icon-180.png", "./cards-data.js?v=20260908h", "./lessons-data.js?v=20260908h"];
 self.addEventListener("install", (e) => {
   e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)).then(() => self.skipWaiting()));
 });
